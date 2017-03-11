89c42ee4c88bab28576996fe99b79dfb7ca9d80e
Kevin Rocker <kevin.j.rocker@gmail.com>
2017-03-10 21:42:55 -0500

    Add Serializers, Config, Classes
    Add the Servant Serializer, integrate it into the model and controller.
    Add the initializer configuration for ActiveModelSerializers, and pass
    both the Servants and the table of ServantClasses to the Index view.

--- app/controllers/servants_controller.rb
+++ app/controllers/servants_controller.rb
@@ -1,6 +1,6 @@
 class ServantsController < ApplicationController
   def index
-    @servants = Servant.all.includes(:servant_class)
+    @servants = Servant.all.includes(:servant_class).map(&:serialize)
     respond_to do |format|
       format.html { render :index }
     end
