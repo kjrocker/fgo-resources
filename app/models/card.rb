2d8504e6331493e011da166ec1f45c18752dfc24
Kevin Rocker <kevin.j.rocker@gmail.com>
2017-05-21 15:55:47 -0400

    Create Tags, via acts_as_taggable_on

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,7 @@
 class Card < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   mount_uploader :portrait, PortraitUploader
+  acts_as_taggable
 
   def self.sorted
     order(official_id: :asc)
