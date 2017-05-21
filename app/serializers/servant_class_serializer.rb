c438b5823f509893cfb295b08f26a650db8f27e2
Kevin Rocker <kevin.j.rocker@gmail.com>
2017-05-21 00:20:43 -0400

    Configuring for Heroku
    Manipulating database.yml file, cleaning up some unused gems, and
    wholesale copying some config files from a freshly generated rails app,
    just in case.

--- app/serializers/servant_class_serializer.rb
+++
@@ -1,9 +0,0 @@
-class ServantClassSerializer < ActiveModel::Serializer
-  include ActionView::Helpers::AssetTagHelper
-
-  attributes :id, :name, :sort_order, :icon_url
-
-  def icon_url
-    asset_path("servant_icons/#{object.icon}")
-  end
-end
